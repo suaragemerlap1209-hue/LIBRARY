f8d65f923189819ccaa2585bd448af01aa3e8d0d
unknown <suaragemerlap1209@mail.com>
2026-07-09 15:47:31 +0800

    feat: baut halaman TENTANG KAMI landing page

---
+++ resources/views/pages/about.blade.php
@@ -0,0 +1,7 @@
+<x-landing.landing-layout title="Perpustakaan Lumina">
+
+    <x-landing.about-tentang-hero />
+    <x-landing.about-nilai-kami />
+    <x-landing.about-cta />
+
+</x-landing.landing-layout>
